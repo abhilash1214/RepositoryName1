dbb739b578e1c8a44933fa3c8d55d8b2c8a0c395
Abilash PL <Abilash PL@DESKTOP-A67D2LQ>
2021-01-12 22:31:34 +0530

    Commit1

---
+++ src/TestClass.java
@@ -0,0 +1,16 @@
+
+//Fabonnic series 0,1,2,3,5,7....
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestClass {
+	public static void main(String[] args) {
+
+		String s = "SELENIUM";
+for(int i=0;i<15; i++) {
+	if(i%2==1)
+	System.out.println(i);
+}
+	}
+
+}
